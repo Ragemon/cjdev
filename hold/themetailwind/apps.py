d5195df4c350b73dfef42b719dd6a3e8e5b448d8
Courage <couragejahmon1@gmail.com>
2022-07-08 22:56:35 +0000

    using main to commit postgres changes

---
+++ hold/themetailwind/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class ThemetailwindConfig(AppConfig):
+    name = 'themetailwind'
--- iknow/settings.py
+++ iknow/settings.py
@@ -37,7 +37,7 @@ INSTALLED_APPS = [
     'taggit',
     'blog.apps.BlogConfig',
     ## Enable in production mode
-    # 'my_user',
+    'my_user',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
