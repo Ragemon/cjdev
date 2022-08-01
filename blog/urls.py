a75c6b558f0ef3d5e4e2910ca90909b351f303a0
Courage <couragejahmon1@gmail.com>
2022-08-01 22:27:13 +0000

    added bloging again

--- blog/urls.py
+++ blog/urls.py
@@ -5,7 +5,7 @@ app_name = 'blog'
 
 urlpatterns = [
         path('', views.ArticleListView.as_view(), name='article_list'),
-        path('en/<int:pk>/<slug:slug>/', views.ArticleDetailView.as_view(), name='article_detail'),
+        path('en/edublog/<int:pk>/<slug:slug>/', views.ArticleDetailView.as_view(), name='article_detail'),
         path('', views.post_update, name='update'),
         path('', views.post_delete, name='delete'),
         path('', views.post_delete, name='detail'),
