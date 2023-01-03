cab1d35e480701b1b431b9c3fafc9889fac75db5
Ragemon <couragejahmon1@gmail.com>
2023-01-03 14:26:30 +0000

    updated the allowed hosts

--- iknow/settings.py
+++ iknow/settings.py
@@ -30,7 +30,7 @@ SECRET_KEY = env("SECRET_KEY")
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = env("DEBUG_BOOL")
 print(type(DEBUG))
-ALLOWED_HOSTS = ['127.0.0.1', 'deepsyntax.org', 'www.deepsyntax.org', '159.223.145.231']
+ALLOWED_HOSTS = ['127.0.0.1', 'deepsyntax.org', 'www.deepsyntax.org', '24.199.106.171']
 
 
 
