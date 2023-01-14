4cb9051aa42c860503a42ccc0d9ff5f7ddb66274
Ragemon <couragejahmon1@gmail.com>
2023-01-14 19:08:52 +0000

    added the robots.txt file

--- iknow/urls.py
+++ iknow/urls.py
@@ -4,7 +4,7 @@ from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
 from django.contrib.staticfiles.urls import staticfiles_urlpatterns
-
+from django.views.generic.base import TemplateView
 
 urlpatterns = []
 if settings.DEBUG == True:
@@ -19,7 +19,7 @@ urlpatterns += [
 
     # path('blog/', include('blog.urls')),
     path('tinymce/', include('tinymce.urls')),
-    
+    path("robots.txt",TemplateView.as_view(template_name="robots.txt", content_type="text/plain")),  #add the robots.txt file
     # automatic browser reloader
     
     
