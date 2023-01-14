4ae968b5e3e71991ad1494d30d2719abbda7b330
Ragemon <couragejahmon1@gmail.com>
2023-01-14 21:56:00 +0000

    removed backslash from url

--- blog/urls.py
+++ blog/urls.py
@@ -7,7 +7,7 @@ urlpatterns = [
         path('blog/snippet', views.SnippetListView.as_view(), name='snippet_list'),
         path('en/snippet/<int:pk>/<slug:slug>/', views.SnippetDetailView.as_view(), name='snippet_detail'),
         path('', views.ArticleListView.as_view(), name='article_list'),
-        path('en/edublog/<int:pk>/<slug:slug>/', views.ArticleDetailView.as_view(), name='article_detail'),
+        path('en/edublog/<int:pk>/<slug:slug>', views.ArticleDetailView.as_view(), name='article_detail'),
         
         path('', views.post_update, name='update'),
         path('', views.post_delete, name='delete'),
